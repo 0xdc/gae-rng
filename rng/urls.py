cfa81f728fa22c370ac04e5f464c9fb54ca501b8
Daniel Cordero (bastion) <bastion@azolla.0xdc.host>
2018-12-16 14:31:19 +0000

    Homepage only on one url

--- rng/urls.py
+++ rng/urls.py
@@ -6,5 +6,5 @@ app_name = "rng"
 urlpatterns = [
     url(r'rng/?$', views.random_number_generator, name="rng"),
     url(r'used/?$', views.usedindex, name="index"),
-    url(r'', views.hpv, name="hp"),
+    url(r'$', views.hpv, name="hp"),
 ]
