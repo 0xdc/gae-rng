c1e076bc070c423be2ad3aaff6bad4d832c52052
Daniel Cordero (bastion) <bastion@azolla.0xdc.host>
2018-12-16 13:57:55 +0000

    Move model logic into models instead of view

--- rng/models.py
+++ rng/models.py
@@ -1,8 +1,27 @@
 from django.db import models
 from djangae import fields
 
+import random
+import functools
+
+_RINT = functools.partial(random.SystemRandom().randint, 0)
+_PRIME = 2 ** 127 - 1
+
 class UsedNumber(models.Model):
     number = fields.CharField()
 
     def __str__(self):
         return self.number
+
+    @classmethod
+    def new(cls):
+        r = str(_RINT(_PRIME))
+        x, c = cls.objects.get_or_create(
+            number = r,
+            defaults = {
+                'number': r
+            },
+        )
+        if c:
+            x.save()
+        return x
--- rng/views.py
+++ rng/views.py
@@ -2,25 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
 
-import random
-import functools
-
 from .models import UsedNumber
 
-_RINT = functools.partial(random.SystemRandom().randint, 0)
-_PRIME = 2 ** 127 - 1
-
 def random_number_generator(request):
-    rand = str(_RINT(_PRIME))
-    UsedNumber.objects.get_or_create(number=rand, defaults={'number': rand})
-    return HttpResponse(rand)
+    rand = UsedNumber.new()
+    return HttpResponse(rand.number)
 
 class HomePageView(TemplateView):
     template_name = "rng/random.html"
 
     def get_context_data(self, **kwargs):
         context = super(HomePageView, self).get_context_data(**kwargs)
-        context['random'] = _RINT(_PRIME)
+        context['random'] = UsedNumber.new().number
         return context
 
 hpv = HomePageView.as_view()
